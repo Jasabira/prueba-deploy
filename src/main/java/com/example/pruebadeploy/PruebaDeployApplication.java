39a92448fa509831be74d44126bbea0efaa1dfdd
jasabira <jasbircoro.dev@gmail.com>
2023-01-16 16:48:01 -0300

    Initial commit

---
+++ src/main/java/com/example/pruebadeploy/PruebaDeployApplication.java
@@ -0,0 +1,14 @@
+package com.example.pruebadeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDeployApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PruebaDeployApplication.class, args);
+    }
+
+}
